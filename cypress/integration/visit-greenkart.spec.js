1e94de36b6b303fb0052d7e36f368e3685e05cb3
Mor Codou GUEYE <morcodou@gmail.com>
2022-01-03 01:50:08 -0500

    Add first test to visit the targeted site

---
+++ cypress/integration/visit-greenkart.spec.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+describe('Browsing GreenKart website', () => {
+
+    it('should open the website url', () => {
+        cy.visit('/seleniumPractise/#/');
+
+
+    });
+    
+})
