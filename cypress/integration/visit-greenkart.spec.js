d58573b3d47000c6457ca3785253f5b6fee250a1
Mor Codou GUEYE <morcodou@gmail.com>
2022-01-03 02:57:41 -0500

    Add search and product results

--- cypress/integration/visit-greenkart.spec.js
+++ cypress/integration/visit-greenkart.spec.js
@@ -3,9 +3,15 @@
 describe('Browsing GreenKart website', () => {
 
     it('should open the website url', () => {
+        const length = 2;
         cy.visit('/seleniumPractise/#/');
 
+        cy.get('.search-keyword').type('Br');
+        cy.wait(2000);
 
+        cy.get('.products').find('.product').should('have.length', length);
+        cy.get('.products > .product').should('have.length', length);
+        cy.get('.product:visible').should('have.length', length);
     });
-    
+
 })
